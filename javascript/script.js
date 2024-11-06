3ba56b57e744495e34c97965da9aa34105b98a85
v <carlosedu1965@hotmail.com>
2024-11-05 21:33:08 -0300

    imagens, infografico, detalhes
    adicionando e estilizando imagens e infografico, modal abre com zoom ao clickar na imagem. melhorando detalhes: hover do menu

--- javascript/script.js
+++ javascript/script.js
@@ -44,3 +44,19 @@ window.onclick = function (event) {
     }
 }
 
+
+//modal 
+
+function openModal(imageSrc) {
+    const modal = document.getElementById("imageModal");
+    const modalImage = document.getElementById("modalImage");
+
+    modal.style.display = "flex";
+    modalImage.src = imageSrc;
+}
+
+function closeModal() {
+    const modal = document.getElementById("imageModal");
+    modal.style.display = "none";
+}
+
